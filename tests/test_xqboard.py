c7529c9b086ecde6d2cea521dd8f56062eb69e19
LDF <liudengfeng_sd@outlook.com>
2023-02-11 10:28:28 +0800

    test lr data

--- tests/test_xqboard.py
+++ tests/test_xqboard.py
@@ -1,5 +1,23 @@
 import xqcpp
 import pytest
+import numpy as np
+
+
+@pytest.mark.parametrize(
+    "init_fen,expected",
+    [
+        (
+            "3ak1NrC/9/4b4/9/2n1c1P2/9/3R5/9/2p6/2BK1A3 r - 0 0 1",
+            "CrN1ka3/9/4b4/9/2P1c1n2/9/5R3/9/6p2/3A1KB2 r - 0 0 1",
+        )
+    ],
+)
+def test_lr_fen(init_fen, expected):
+    board = xqcpp.XqBoard()
+    board.init_set(init_fen, True)
+    actual = board.get_lr_fen()
+    # print(actual)
+    assert expected == actual
 
 
 @pytest.mark.parametrize(
@@ -29,3 +47,25 @@ def test_2d(init_fen, expected):
     actual = board.get2d()
     # print(actual)
     assert expected == actual
+
+
+@pytest.mark.parametrize(
+    "init_fen,lr_fen",
+    [
+        (
+            "3ak1NrC/9/4b4/9/2n1c1P2/9/3R5/9/2p6/2BK1A3 r - 0 0 1",
+            "CrN1ka3/9/4b4/9/2P1c1n2/9/5R3/9/6p2/3A1KB2 r - 0 0 1",
+        )
+    ],
+)
+def test_lr_fen_2d(init_fen, lr_fen):
+    board = xqcpp.XqBoard()
+    board.init_set(init_fen, True)
+    actual = board.get2d(True)
+    b2 = xqcpp.XqBoard()
+    b2.init_set(lr_fen, True)
+    data = b2.get2d()
+    np.testing.assert_array_equal(data, actual)
+
+    lr_data = b2.get2d(True)
+    np.testing.assert_array_equal(np.fliplr(data), lr_data)
